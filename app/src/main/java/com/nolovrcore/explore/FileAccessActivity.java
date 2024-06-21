dd7042526c7dced66a328225ab6af41891f8f4e1
msi-liu.lg@163.com <liu.lg@163.com>
2024-06-21 17:11:11 +0800

    准备尝试 FileProvider

--- app/src/main/java/com/nolovrcore/explore/FileAccessActivity.java
+++ app/src/main/java/com/nolovrcore/explore/FileAccessActivity.java
@@ -1,24 +1,99 @@
 package com.nolovrcore.explore;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.FileUtils;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.nolovrcore.explore.utils.FileUtil;
+
+import java.io.File;
+
 public class FileAccessActivity extends AppCompatActivity {
+
+    private static final String TAG = "_FileAccessActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_file_access);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
-//        });
+//        }
+
+
+        
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+
+        String downloadFilePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+        Log.d(TAG, "onResume: downloadFilePath="+downloadFilePath);
+
+        File pathname = Environment.getExternalStorageDirectory();
+        File directory_download = new File(pathname, "Download");
+        Log.e(TAG, "directory_download = " + directory_download.getAbsolutePath());
+
+        String targetPath =downloadFilePath +  "/"+ "gs3d/"+"runtime.config";
+
+        File file = new File(targetPath);
+        if (file.exists()){
+            Log.d(TAG, "onResume: ok 1  "+ targetPath);
+
+            try {
+                String fileContent = FileUtil.getFileContent(file);
+                Log.d(TAG, "onResume: fileContent="+fileContent);
+            } catch (Exception e) {
+                Log.e(TAG, "onResume: ---》", e);
+            }
+
+
+        }else {
+            Log.d(TAG, "onResume: fail 1  "+ targetPath);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    public void goFile(View view) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+//        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        startActivityForResult(intent, 10087);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: ");
     }
 }
---
+++ app/src/main/java/com/nolovrcore/explore/utils/FileUtil.java
@@ -0,0 +1,106 @@
+package com.nolovrcore.explore.utils;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+
+public class FileUtil {
+
+    private static final String TAG = "FileUtil";
+
+    private void writeData() {
+        String filePath = "/sdcard/Gyt/";
+        String fileName = "data.txt";
+        writeTxtToFile("Wx:lcti1314", filePath, fileName);
+    }
+
+    // 将字符串写入到文本文件中
+    public static void writeTxtToFile(String strcontent, String filePath, String fileName) {
+        //生成文件夹之后，再生成文件，不然会出错
+        makeFilePath(filePath, fileName);
+
+        String strFilePath = filePath + fileName;
+        // 每次写入时，都换行写
+        String strContent = strcontent + "\r\n";
+        try {
+            File file = new File(strFilePath);
+            if (!file.exists()) {
+                Log.d(TAG, "Create the file:" + strFilePath);
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+            RandomAccessFile raf = new RandomAccessFile(file, "rwd");
+            raf.seek(file.length());
+            raf.write(strContent.getBytes());
+            raf.close();
+        } catch (Exception e) {
+            Log.e(TAG, "Error on write File:" + e);
+        }
+    }
+
+//生成文件
+
+    private static File makeFilePath(String filePath, String fileName) {
+        File file = null;
+        makeRootDirectory(filePath);
+        try {
+            file = new File(filePath + fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+//生成文件夹
+
+    private static void makeRootDirectory(String filePath) {
+        File file = null;
+        try {
+            file = new File(filePath);
+            if (!file.exists()) {
+                file.mkdir();
+            }
+        } catch (Exception e) {
+            Log.i("error:", e + "");
+        }
+    }
+
+
+    //---------------------
+    //读取指定目录下的所有TXT文件的文件内容
+    public static String getFileContent(File file) {
+        String content = "";
+        if (!file.isDirectory()) {  //检查此路径名的文件是否是一个目录(文件夹)
+            if (file.getName().endsWith("txt") || file.getName().endsWith("config")) {//文件格式为""文件
+                try {
+                    InputStream instream = new FileInputStream(file);
+                    if (instream != null) {
+                        InputStreamReader inputreader
+                                = new InputStreamReader(instream, "UTF-8");
+                        BufferedReader buffreader = new BufferedReader(inputreader);
+                        String line = "";
+                        //分行读取
+                        while ((line = buffreader.readLine()) != null) {
+                            content += line + "\n";
+                        }
+                        instream.close();//关闭输入流
+                    }
+                } catch (java.io.FileNotFoundException e) {
+                    Log.d(TAG, "The File doesn't not exist. " + e.getMessage());
+                } catch (IOException e) {
+                    Log.d(TAG, e.getMessage());
+                }
+            }
+        }
+        return content;
+    }
+}
