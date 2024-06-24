77e10f1b2dcc7c726b9dc0055b14bd3760dfb57b
msi-liu.lg@163.com <liu.lg@163.com>
2024-06-24 14:06:00 +0800

    尝试 uri 文件读取失败

--- app/src/main/java/com/nolovrcore/explore/FileAccessActivity.java
+++ app/src/main/java/com/nolovrcore/explore/FileAccessActivity.java
@@ -1,6 +1,8 @@
 package com.nolovrcore.explore;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.FileUtils;
@@ -17,6 +19,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.nolovrcore.explore.utils.FileUtil;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class FileAccessActivity extends AppCompatActivity {
 
@@ -35,7 +40,6 @@ public class FileAccessActivity extends AppCompatActivity {
 //        }
 
 
-        
     }
 
     @Override
@@ -43,29 +47,61 @@ public class FileAccessActivity extends AppCompatActivity {
         super.onResume();
         Log.d(TAG, "onResume: ");
 
+
+        String uriStr = "content://com.fish.easystorage.easy.fileprovider/inner_app_file/myfile/myTxt.txt";
+        Uri uri = Uri.parse(uriStr);
+        if (uri != null) {
+            Log.d(TAG, "onResume: --------uri = " + uri.getAuthority());
+
+
+            ContentResolver contentResolver = getContentResolver();
+            InputStream inputstream = null;
+            try {
+                inputstream = contentResolver.openInputStream(uri);
+                //读取文件内容
+
+            } catch (FileNotFoundException e) {
+                Log.e(TAG, "FileNotFoundException onResume: ", e);
+            } finally {
+                if (inputstream != null) {
+                    try {
+                        inputstream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+
+
+                    }
+                }
+            }
+
+        } else {
+            Log.d(TAG, "onResume: uri = null");
+        }
+
+
         String downloadFilePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
-        Log.d(TAG, "onResume: downloadFilePath="+downloadFilePath);
+        Log.d(TAG, "onResume: downloadFilePath=" + downloadFilePath);
 
         File pathname = Environment.getExternalStorageDirectory();
         File directory_download = new File(pathname, "Download");
         Log.e(TAG, "directory_download = " + directory_download.getAbsolutePath());
 
-        String targetPath =downloadFilePath +  "/"+ "gs3d/"+"runtime.config";
+        String targetPath = downloadFilePath + "/" + "gs3d/" + "runtime.config";
 
         File file = new File(targetPath);
-        if (file.exists()){
-            Log.d(TAG, "onResume: ok 1  "+ targetPath);
+        if (file.exists()) {
+            Log.d(TAG, "onResume: ok 1  " + targetPath);
 
             try {
                 String fileContent = FileUtil.getFileContent(file);
-                Log.d(TAG, "onResume: fileContent="+fileContent);
+                Log.d(TAG, "onResume: fileContent=" + fileContent);
             } catch (Exception e) {
                 Log.e(TAG, "onResume: ---》", e);
             }
 
 
-        }else {
-            Log.d(TAG, "onResume: fail 1  "+ targetPath);
+        } else {
+            Log.d(TAG, "onResume: fail 1  " + targetPath);
         }
 
     }
